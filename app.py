16b2a0ccb7878e8df8b12a4d6702701a39ffee6c
BHAKAREP17 <bhakareprashant17@gmail.com>
2026-03-06 11:04:50 +0530

    calculator project

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+   return "Hello Upasana! Docker is running on port 5000 🚀"
+
+
+if __name__ == "__main__":
+   # Important: host=0.0.0.0 so container can expose it outside
+   app.run(host="0.0.0.0", port=5000)
